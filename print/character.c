3abfe6cca7636cec1dd24cea1fe5c0d95f23e1ba
mhahn <mhahn@student.42heilbronn.de>
2021-12-01 22:32:21 +0100

    Optimized unused variable

--- print/character.c
+++ print/character.c
@@ -2,8 +2,7 @@
 
 #include "./print.h"
 
-size_t	print_character(t_format *f, char c)
+size_t	print_character(__attribute__((unused)) t_format *f, char c)
 {
-	f = NULL;
 	return (write(1, &c, 1));
 }
