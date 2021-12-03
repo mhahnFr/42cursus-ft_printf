b647e5f46de956b7eb2a7ddee96d211c9c930945
mhahn <mhahn@student.42heilbronn.de>
2021-12-03 19:32:16 +0100

    Fix

--- print/hex.c
+++ print/hex.c
@@ -1,7 +1,7 @@
 #include "print.h"
 #include "../print_helper/print_helper.h"
 
-size_t	print_hex(__attribute__((unsused))t_format *f, int hex, bool uppercase)
+size_t	print_hex(__attribute__((unused))t_format *f, int hex, bool uppercase)
 {
 	char	*base;
 
